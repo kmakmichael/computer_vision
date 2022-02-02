69c6d2e24e867c7b18c726bd3aeeaf0310e21fe9
kmakmichael <m_kmak@u.pacific.edu>
2022-02-02 04:36:50 -0800

    floodfill with frontier

--- unit_1/functions/floodfill_f.cpp
+++ unit_1/functions/floodfill_f.cpp
@@ -13,6 +13,15 @@ bool floodfill_f(cv::Mat &img, cv::Point2i seed, uchar fill_color) {
     while (frontier.size() != 0) {
         cv::Point2i q = frontier.back();
         frontier.pop_back();
+        for (int r = -1; r < 2; r++) {
+            for (int c = -1; c < 2; c++) {
+                cv::Point2i n = q + cv::Point2i(r, c);
+                if (img.at<uchar>(n) == old_color) {
+                    frontier.push_back(n);
+                    img.at<uchar>(n) = fill_color;
+                }
+            }
+        }
     }
     
     return true;
