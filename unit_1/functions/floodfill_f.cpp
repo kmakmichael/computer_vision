2a2736879cd19c9ebc5234612a02b5fdcd2472a4
kmakmichael <m_kmak@u.pacific.edu>
2022-02-02 04:16:20 -0800

    create frontier

--- unit_1/functions/floodfill_f.cpp
+++ unit_1/functions/floodfill_f.cpp
@@ -1,5 +1,15 @@
 #include "floodfill_f.hpp"
 
-bool floodfill_f(cv::Mat &img) {
+bool floodfill_f(cv::Mat &img, cv::Point_<uchar> seed, uchar fill_color) {
+    std::vector<cv::Point_<uchar>> frontier;
+    cv::Point_<uchar> up(-1,0);
+    cv::Point_<uchar> down(1,0);
+    cv::Point_<uchar> left(0,-1);
+    cv::Point_<uchar> right(0,1);
+
+    uchar old_color = img.at<uchar>(seed);
+    frontier.push_back(seed);
+
+    std::printf("Seed is %d at (%d, %d), front has %zu members\n", old_color, seed.x, seed.y, frontier.size());
     return true;
 }
--- unit_1/functions/floodfill_f.hpp
+++ unit_1/functions/floodfill_f.hpp
@@ -2,8 +2,9 @@
 #define FLOODFILLF_H
 
 #include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
 
 
-bool floodfill_f(cv::Mat &img);
+bool floodfill_f(cv::Mat &img, cv::Point_<uchar> seed, uchar fill_color);
 
 #endif
