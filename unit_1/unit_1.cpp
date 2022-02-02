4d2cb54e74c91008ea520074f3082af24ffd5589
kmakmichael <m_kmak@u.pacific.edu>
2022-02-02 01:34:35 -0800

    program arguments, restructure

---
+++ unit_1/unit_1.cpp
@@ -0,0 +1,53 @@
+#include <opencv2/opencv.hpp>
+#include <cstdio>
+#include <cstring>
+
+#include "functions/histeq.hpp"
+
+/*
+ * Test program for 7 different pixel processing functions
+ *  usage: test <function> <input> <output>
+ *
+ *  functions:
+ *      histeq: histogram equalization
+*/
+int main(int argc, char *argv[]) {
+    bool result;
+
+    // handle params
+	if (argc != 4) {
+		fprintf(stderr, "usage: test <function> <input> <output>\n");
+		return 1;
+	}
+
+    char *func = argv[1];
+    char *file_in = argv[2];
+    char *file_out = argv[3];
+
+    // read the given image
+    cv::Mat img = cv::imread(file_in, cv::IMREAD_GRAYSCALE);
+    if  (img.empty()) {
+        return 1;
+    }
+    
+    // call the proper function
+    if (!strcmp(func, "histeq")) {
+        fprintf(stdout, "running histeq\n");
+        result = histeq(img);
+    } else {
+        fprintf(stderr, "%s is not a valid function\n", func);
+        return 1;
+    }
+
+    // error checking
+    if (!result) {
+        fprintf(stderr, "unable to perform %s on %s", func, file_in);
+    }
+
+    // write the result
+    result = cv::imwrite(file_out, img);
+    if (!result) {
+        return 1;
+    }
+    return 0;
+}
