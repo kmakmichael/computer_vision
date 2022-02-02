0fdbbea7461f3e090c65123672fb6d11f00a0b7d
kmakmichael <m_kmak@u.pacific.edu>
2022-02-02 03:05:06 -0800

    comment on histeq

--- unit_1/functions/histeq.hpp
+++ unit_1/functions/histeq.hpp
@@ -5,7 +5,15 @@
 #include <opencv2/core/mat.hpp>
 #include <cstdint>
 
-// performs histogram equalization on an image
+/*
+    performs histogram equalization on an image
+ 
+    notes:
+        could benefit from use of the Mat.setTo() function. Changing
+        an array of pixels in parallel might be worth the overhead of
+        storing a matrix of pixels with that value (pdf would be as 
+        big as the original image at minimum to store all the indices)
+ */
 bool histeq(cv::Mat &img);
 
 #endif
