4706c0a0fc800caa820c7da78f987e9a3ed9395e
kmakmichael <m_kmak@u.pacific.edu>
2022-02-01 23:36:39 -0800

    improved read/write

--- unit_1/histeq.cpp
+++ unit_1/histeq.cpp
@@ -1,12 +1,23 @@
 #include<opencv2/opencv.hpp>
 
+#define file_out "out.bmp"
+#define file_in "images/fruit1.bmp"
+
+
 using namespace std;
 using namespace cv;
 
 int main() {
-    Mat grey = imread("images/fruit1.bmp", 0);
-    namedWindow("some fruits", WINDOW_AUTOSIZE );
-    imshow("some fruits", grey);
-    waitKey(0);
+    bool result;
+
+    Mat grey = imread(file_in, IMREAD_GRAYSCALE);
+    if  (grey.empty()) {
+        cout << "Failed to load " << file_in << endl;
+    }
+
+    result = imwrite(file_out, grey);
+    if (!result) {
+        cout << "Failed to write " << file_out << endl;
+    }
     return 0;
 }
